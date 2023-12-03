7061fb952e53771ae916693506de624f364b8f16
TeluguZone <140744388+TeluguZone@users.noreply.github.com>
2023-12-04 01:22:11 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -245,11 +245,11 @@ Tɪᴘ: Sᴇʟᴇᴄᴛ "Lᴀɴɢᴜᴀɢᴇs" ᴏʀ "Sᴇᴀsᴏɴs" Bᴜᴛᴛ
 
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
-    CAPTION = """<b>📂Fɪʟᴇɴᴀᴍᴇ : {filename}
+    CAPTION = """<b>📂Fɪʟᴇɴᴀᴍᴇ : {file_name}
 
 <b> Size ⚙️: {file_size}
 
-</b>""" 
+JOIN:[TeluguZone](https://t.me/TeluguZone0)</b>""" 
 
     IMDB_TEMPLATE_TXT = """
 <b>Query: {qurey}
